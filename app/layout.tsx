2a322908ee0c22ef340bfb0832d576676a460ddd
Waleed Akhter <waleeakhter@gmail.com>
2023-09-19 01:32:39 +0100

    remove meta info from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 'use client'
-import type { Metadata } from 'next'
+// import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -12,10 +12,10 @@ import { Hydrate } from '@tanstack/react-query';
 import React from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'JK',
-  description: 'Developed By Waleed Akhter',
-}
+// export const metadata: Metadata = {
+//   title: 'JK',
+//   description: 'Developed By Waleed Akhter',
+// }
 
 export default function RootLayout({
   children,
