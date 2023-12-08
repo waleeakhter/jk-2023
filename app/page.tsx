97c54c1ef39c38fb531980ccbec484e3b136ebad
Waleed Akhter <waleeakhter@gmail.com>
2023-12-08 19:46:55 +0000

    DASHBORD WORK

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { redirect } from 'next/navigation'
 import { auth } from './auth'
+import SaleChart from './Dashboard/SaleChart'
+import { getMonthlySalesAndProfit } from './Dashboard/ServerActions'
 
 const Home = async () => {
-    const session = await auth()
-    if (!session?.user?.email) {
-        return redirect('/api/auth/signin')
-    }
-    return (
+  const session = await auth()
+  if (!session?.user?.email) {
+    return redirect('/api/auth/signin')
+  }
 
-        <h1>{JSON.stringify(session, null, 2)}</h1>
-
-    )
+  return (
+    <>
+      <h1>{JSON.stringify(session, null, 2)}</h1>
+      <SaleChart />
+    </>
+  )
 }
 
 export default Home
