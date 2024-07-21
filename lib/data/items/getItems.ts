da43af0aa4ef229c40ce235e0d739cbff7c25785
Waleed Akhter <waleeakhter@gmail.com>
2024-07-21 22:16:37 +0100

    Refactor date handling in MobileViewList component

--- lib/data/items/getItems.ts
+++ lib/data/items/getItems.ts
@@ -1,35 +1,40 @@
 import Item from "@/models/Item";
 import dbConnect from "@/utils/dbConnect";
 
- const fetchItems = async (searchParams  : any) => {
-    const {name , type  = "lcd"} = searchParams || {};
-    await  dbConnect();
-    const res = await Item.find(
-      { 
-        ...(name && { "name": { $regex: decodeURIComponent(name.toString()), $options: "i" }}),
-        type: type ,   $or: [
-          { stock: { $gt: 0 } },
-          { wearHouseStock: { $gt: 0 } }
-      ], },
-      { name: 1, price: 1, _id: 1, type: 1, stock: 1, wearHouseStock: 1 }
-    )
-      .sort({ name: 1 })
-      .collation({ locale: "en" })
-      .lean();
+const fetchItems = async (searchParams: any) => {
+  const { name, type = "lcd" } = searchParams || {};
+  let greaterThanZero = type !== "lcd";
 
-    const customSort = function (a: any, b: any): number {
-        const nameA = (a.name as string).toUpperCase();
-        const nameB = (b.name as string).toUpperCase();
+  await dbConnect();
+  const res = await Item.find(
+    {
+      ...(name && {
+        name: { $regex: decodeURIComponent(name.toString()), $options: "i" },
+      }),
+      type: type,
+      ...(greaterThanZero && { stock: { $gt: 0 } }),
+    },
+    { name: 1, price: 1, _id: 1, type: 1, stock: 1, wearHouseStock: 1 }
+  )
+    .sort({ name: 1 })
+    .collation({ locale: "en" })
+    .lean();
 
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    };
-    const _res = res.length ? res.map((item) => ({...item,_id : (item._id as string).toString() , })) : []
-    return _res.sort(customSort);
+  const customSort = function (a: any, b: any): number {
+    const nameA = (a.name as string).toUpperCase();
+    const nameB = (b.name as string).toUpperCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
   };
-  export default fetchItems
+  const _res = res.length
+    ? res.map((item) => ({ ...item, _id: (item._id as string).toString() }))
+    : [];
+  return _res.sort(customSort);
+};
+export default fetchItems;
